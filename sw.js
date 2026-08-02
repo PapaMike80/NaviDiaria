e7e4f079a94c6a7f3c01af74fbb870d8689b7c4e
PapaMike80 <70916601+PapaMike80@users.noreply.github.com>
2026-08-02 10:34:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - provare sempre prima la rete e usare la cache solo come fallback.
  */
 
-const CACHE_VERSION = 'navisuite-v77';
+const CACHE_VERSION = 'navisuite-v78';
 const CACHE_NAME = CACHE_VERSION;
 
 // File statici da pre-caricare durante l'installazione.
